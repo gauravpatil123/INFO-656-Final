25240c6541c9f695bb81f228f2381bab650e5083
gauravpatil123 <gaurav.patil123@gmail.com>
2023-12-02 16:18:13 -0500

    initialised selected style & content image path str

--- main_app.py
+++ main_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from PIL import Image
 import os
+# from InSt import generate_images as gen
 
 MODERN_EMBEDDING_PATH = "./embeddings/modern_embeddings.pt"
 ANDRE_EMBEDDING_PATH = "./embeddings/andre-derain_embeddings.pt"
-style_path_1 = "./assets/style_images/modern.jpg"
-style_path_2 = "./assets/style_images/andre-derain.jpg"
+style_modern = "./assets/style_images/modern.jpg"
+style_andre = "./assets/style_images/andre-derain.jpg"
 CONTENT_IMG_DIR = "./assets/content_images/"
 current_content_img_path = ""
 
@@ -26,6 +27,16 @@ def style_embedding_path(style_choice):
         path = ANDRE_EMBEDDING_PATH
     else:
         path = MODERN_EMBEDDING_PATH
+    return path
+
+def style_image_path(style_choice):
+    path = ""
+    if style_choice == "Modern":
+        path = style_modern
+    elif style_choice == "Andre Derain":
+        path = style_andre
+    else:
+        path = style_modern
     return path
 
 def save_uploaded_image(dir, uploaded_image):
@@ -49,8 +60,8 @@ if __name__=="__main__":
 
     st.write(intro_str)
 
-    style_image_1 = load_img_from_path(style_path_1)
-    style_image_2 = load_img_from_path(style_path_2)
+    style_image_1 = load_img_from_path(style_modern)
+    style_image_2 = load_img_from_path(style_andre)
 
     style_str = "## Choose a Style Image"
     st.write(style_str)
@@ -72,10 +83,14 @@ if __name__=="__main__":
     style_selected = style_prompt(style_choice)
 
     embedding_path = style_embedding_path(style_choice)
+    style_image_p = style_image_path(style_choice)
 
-    content_img = st.file_uploader(label='Upload a content Image', type=['png', 'jpg'])
+    content_img = st.file_uploader(label='#### Upload a content Image', type=['png', 'jpg'])
 
     if content_img:
         save_uploaded_image(CONTENT_IMG_DIR, content_img)
+        content_image_p = current_content_img_path
+
+
 
 
