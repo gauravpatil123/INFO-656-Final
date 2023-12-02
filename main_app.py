35204970da35b225f9f0a009d80576b281ac6aaa
gauravpatil123 <gaurav.patil123@gmail.com>
2023-12-02 15:05:36 -0500

    updated app structure in main_app => __main__ function

--- main_app.py
+++ main_app.py
@@ -1,61 +1,23 @@
-"""
-Imports
-"""
 import streamlit as st
 from PIL import Image
 
-"""
-Variables
-"""
 MODERN_EMBEDDING_PATH = "./embeddings/modern_embeddings.pt"
 ANDRE_EMBEDDING_PATH = "./embeddings/andre-derain_embeddings.pt"
+style_path_1 = "./assets/style_images/modern.jpg"
+style_path_2 = "./assets/style_images/andre-derain.jpg"
 
 intro_str = """
-# My first app 
-Hello *world!*  
-InST Interface
+# My InST Feedback App 
 """
-
-st.write(intro_str)
-
-content_img = st.sidebar.file_uploader(label='Upload a content Image', type=['png', 'jpg'])
-
-# style_img = st.sidebar.file_uploader(label='Upload a style Image', type=['png', 'jpg'])
 
 def load_img_from_path(path):
     img = Image.open(path)
     return img
 
-sp1 = "./assets/style_images/modern.jpg"
-sp2 = "./assets/style_images/andre-derain.jpg"
-si1 = load_img_from_path(sp1)
-si2 = load_img_from_path(sp2)
-
-style_str = "## Choose a Style Image"
-st.write(style_str)
-
-col1, col2 = st.columns(2)
-
-with col1:
-    dsi1 = st.image(si1, width=300)
-    st.write("### Modern")
-    #modern = st.checkbox("Modern")
-
-with col2:
-    dsi2 = st.image(si2, width=460)
-    st.write("### Andre Derain")
-    #andre = st.checkbox("Andre Derain")
-
-style_choice = st.radio(
-                "#### Choose a Style Image",
-                ["Modern", "Andre Derain"],)
-
 def style_prompt(style):
     out = f"###### {style} style selected"
     st.write(out)
     return out
-
-style_selected = style_prompt(style_choice)
 
 def style_embedding_path(style_choice):
     path = ""
@@ -67,8 +29,38 @@ def style_embedding_path(style_choice):
         path = MODERN_EMBEDDING_PATH
     return path
 
-embedding_path = style_embedding_path(style_choice)
+if __name__=="__main__":
+    
+    st.write(intro_str)
 
-if content_img:
-    img = Image.open(content_img)
-    st.image(img, width=500)
+    content_img = st.sidebar.file_uploader(label='Upload a content Image', type=['png', 'jpg'])
+    #style_img = st.sidebar.file_uploader(label='Upload a style Image', type=['png', 'jpg'])
+
+    style_image_1 = load_img_from_path(style_path_1)
+    style_image_2 = load_img_from_path(style_path_2)
+
+    style_str = "## Choose a Style Image"
+    st.write(style_str)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        dsi1 = st.image(style_image_1, width=300)
+        st.write("### Modern")
+
+    with col2:
+        dsi2 = st.image(style_image_2, width=460)
+        st.write("### Andre Derain")
+
+    style_choice = st.radio(
+                "#### Choose a Style Image",
+                ["Modern", "Andre Derain"],)
+    
+    style_selected = style_prompt(style_choice)
+
+    embedding_path = style_embedding_path(style_choice)
+
+    if content_img:
+        img = Image.open(content_img)
+        st.image(img, width=500)
+
