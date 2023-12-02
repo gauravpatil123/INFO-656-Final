aa5b28ed7a3aa97048ff47aa8dcff271ee730632
gauravpatil123 <gaurav.patil123@gmail.com>
2023-12-01 22:23:28 -0500

    initialised style slection interface

---
+++ main_app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from PIL import Image
+
+intro_str = """
+# My first app 
+Hello *world!*  
+InST Interface
+"""
+
+st.write(intro_str)
+
+content_img = st.sidebar.file_uploader(label='Upload a content Image', type=['png', 'jpg'])
+
+style_img = st.sidebar.file_uploader(label='Upload a style Image', type=['png', 'jpg'])
+
+def load_img_from_path(path):
+    img = Image.open(path)
+    return img
+
+sp1 = "./assets/style_images/modern.jpg"
+sp2 = "./assets/style_images/andre-derain.jpg"
+si1 = load_img_from_path(sp1)
+si2 = load_img_from_path(sp2)
+
+style_str = "## Choose a Style Image"
+st.write(style_str)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    dsi1 = st.image(si1, width=300)
+    modern = st.checkbox("Modern")
+
+with col2:
+    dsi2 = st.image(si2, width=460)
+    andre = st.checkbox("Andre Derain")
+
+if modern:
+    st.write("Modern Style Selected")
+
+if andre:
+    st.write("Andre Derian Style Selected")
+
+def style_embedding_path():
+    path = ""
+    if modern:
+        path = ""
+    if andre:
+        path = ""
+    return path
+
+
+if content_img:
+    img = Image.open(content_img)
+    st.image(img, width=500)
